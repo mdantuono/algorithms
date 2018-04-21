ca81c15d8a9ed8b3539830dea2347dcfd347a51a
Michael DAntuono <mikeishere3@gmail.com>
2018-04-21 15:57:37 +0000

    string contains altered

--- strContains.c
+++ strContains.c
@@ -13,7 +13,8 @@ int main(int argc, char* argv[])
 
     // printf("%s %s\n", one, two);
 
-    int count = 0;
+    int count = 0; // count to iterate through letters of 'two'
+    int othercount = 0; // count to count how many times string appears
 
     for (int i = 0; i < strlen(one); i++)
     {
@@ -21,16 +22,22 @@ int main(int argc, char* argv[])
         {
             if (two[count + 1] == '\0')
             {
+                if (othercount > 0)
+                {
+                    printf("This string exists more than once\n");
+                    return 2;
+                }
+
+                othercount++;
                 printf("This string exists\n");
                 return 1;
+
             }
             else
             {
                 count++;
             }
-
         }
-
     }
 
     printf("This string does not exist\n");
